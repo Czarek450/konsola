af1ae06de2835c060dfb46f352898f9d9ff8b93b
Cezary450 <cezary45000@wp.pl>
2020-12-03 19:49:42 +0100

    Add project files.

---
+++ konsola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace konsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 3;
+            int c = a + b;
+
+
+
+
+
+            Console.WriteLine(c);
+        }
+    }
+}
