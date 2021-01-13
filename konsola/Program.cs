355fe021d46fad0323ef990da0b112a38302bf27
cezary45000@wp.pl <cezary45000@wp.pl>
2021-01-13 12:08:41 -0800

    dodawanie odejmowanie mnozenie

--- konsola/Program.cs
+++ konsola/Program.cs
@@ -6,15 +6,30 @@ namespace konsola
     {
         static void Main(string[] args)
         {
-            int a = 4;
-            int b = 3;
-            int c = a + b;
+            Console.WriteLine("Wpisz liczbe l");
+            int j = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Wpisz liczbe K");
+            int k = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Wybierz Działanie");
+            string g = Console.ReadLine();
+            
+            if (g == "-")
+            {
+                Console.WriteLine(j-k);
+            }
+            else if (g == "+")
+            {
+                int x = j + k;
+                Console.WriteLine(x);
+            }
+            else if(g == "*")
+            {
+                Console.WriteLine(j*k);
+            }
 
-
-
-            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
